aa3715a5dea50ebae5830d69e3d49d5c5772fecb
kabirdeula <kabirdeula167@gmail.com>
2022-05-26 20:02:17 +0545

    inserted 2 datas

--- database/data.sql
+++ database/data.sql
@@ -12,3 +12,7 @@ CREATE TABLE student(
     DOB DATE NOT NULL,
     CreatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO student(FirstName, MiddleName, LastName, Gender, PermanentAddress, TemporaryAddress, Mobile, Email, DOB) VALUES
+('Frank', 'Lloyd', 'Wright', 'Male', 'Richland Center, Wisconsin, U.S.', 'Phoenix, Arizona, U.S.', '9887186791', 'frankwright67@gmail.com', '1867-06-08'),
+('Mary','' ,'Borthwick', 'Female', 'Boone, Iowa, U.S.', 'Spring Green, Wisconsin, U.S.', '9881906186', 'maryborthwick69@gmail.com', '1869-06-19');
