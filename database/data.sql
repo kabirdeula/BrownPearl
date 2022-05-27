5b95b40d9c3967aafc86b890ab11271672c0db7c
kabirdeula <kabirdeula167@gmail.com>
2022-05-27 15:31:31 +0545

    added father values

--- database/data.sql
+++ database/data.sql
@@ -61,3 +61,7 @@ CREATE TABLE spouse(
 INSERT INTO student(FirstName, MiddleName, LastName, Gender, PermanentAddress, TemporaryAddress, Mobile, Email, DOB) VALUES
 ('Frank', 'Lloyd', 'Wright', 'Male', 'Richland Center, Wisconsin, U.S.', 'Phoenix, Arizona, U.S.', '9887186791', 'frankwright67@gmail.com', '1867-06-08'),
 ('Mary','' ,'Borthwick', 'Female', 'Boone, Iowa, U.S.', 'Spring Green, Wisconsin, U.S.', '9881906186', 'maryborthwick69@gmail.com', '1869-06-19');
+
+INSERT INTO father VALUES
+(1, 'William', 'Cary', 'Wright', 'Musician', 'Massachusetts', '9881825190'),
+(2, 'Marcus', 'Smith', 'Borthwick', 'Engineer', 'Boone', '9880912518');
