a762389cd5d047dd18605e22a77d98a4641659eb
kabirdeula <kabirdeula167@gmail.com>
2022-05-28 14:18:49 +0545

    added footer

---
+++ admin/footer.php
@@ -0,0 +1,21 @@
+        <!-- Footer -->
+        <footer class="sticky-footer bg-white">
+            <div class="container my-auto">
+                <div class="copyright text-center my-auto">
+                    <span>Copyright &copy; Brown Pearl 2022 - <?php echo date("Y");?></span>
+                </div>
+            </div>
+        </footer>
+        <!-- End of Footer -->
+    </div>
+</div>
+
+<!-- Bootstrap core JavaScript-->
+<script src="./vendor/jquery/jquery.min.js"></script>
+<script src="./vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="./vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="./js/app.min.js"></script>
