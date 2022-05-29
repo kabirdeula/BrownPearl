a2332617163327d01e1a089f8589d3087b9c9a20
kabirdeula <kabirdeula167@gmail.com>
2022-05-29 14:47:21 +0545

    updated dashboard

--- admin/index.php
+++ admin/index.php
@@ -8,6 +8,13 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     exit;
 }
 
+require_once "../assets/php/config.php";
+
+$sqlStudent = $pdo -> prepare("SELECT * FROM student");
+$sqlStudent -> execute();
+
+$studentCount = $sqlStudent -> rowCount();
+
 ?>
 
 <!DOCTYPE html>
@@ -17,16 +24,46 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <style>
-        body{ font: 14px sans-serif; text-align: center; }
-    </style>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./vendor/fontawesome-free/css/all.min.css">
 </head>
-<body>
-    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]);?></b>. Welcome to our site.</h1>
-    <p>
-        <a href="./reset-password.php" class="btn btn-warning">Reset Your Password</a>
-        <a href="./logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
-    </p>
+<body id="page-top">
+    <?php include 'header.php';?>
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+        </div>
+
+        <div class="row">
+
+            <!-- Student Card -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                    Students
+                                </div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                    <?php echo $studentCount;?>
+                                </div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-graduation-cap fa-2x text-primary"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+    </div>
+</div>
+    <?php include 'footer.php';?>
 </body>
 </html>
