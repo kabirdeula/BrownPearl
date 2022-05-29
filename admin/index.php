0d12240e1c38d8c750c41ebfcd933943c0a051e9
kabirdeula <kabirdeula167@gmail.com>
2022-05-29 22:38:09 +0545

    minor fix

--- admin/index.php
+++ admin/index.php
@@ -4,7 +4,7 @@ session_start();
 
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
-    header("location: ./index.php");
+    header("location: ../index.php");
     exit;
 }
 
