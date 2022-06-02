99f0492fae809dd62f5ae709a25d6550cbf509ad
kabirdeula <kabirdeula167@gmail.com>
2022-06-02 19:27:28 +0545

    updated Student Sidebar

--- admin/header.php
+++ admin/header.php
@@ -82,33 +82,31 @@ $activePage = basename($_SERVER["PHP_SELF"],".php");
         </div> -->
         
         <!-- Nav Item - Pages Collapse Menu -->
-        <!-- <li class="nav-item active"> -->
-            <!-- <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" -->
-                <!-- aria-controls="collapsePages"> -->
-                <!-- <i class="fas fa-fw fa-folder"></i> -->
-                <!-- <span>Pages</span> -->
-            <!-- </a> -->
-            <!-- <div id="collapsePages" class="collapse show" aria-labelledby="headingPages" -->
-                <!-- data-parent="#accordionSidebar"> -->
-                <!-- <div class="bg-white py-2 collapse-inner rounded"> -->
-                    <!-- <h6 class="collapse-header">Login Screens:</h6> -->
-                    <!-- <a class="collapse-item" href="login.html">Login</a> -->
-                    <!-- <a class="collapse-item" href="register.html">Register</a> -->
-                    <!-- <a class="collapse-item" href="forgot-password.html">Forgot Password</a> -->
-                    <!-- <div class="collapse-divider"></div> -->
-                    <!-- <h6 class="collapse-header">Other Pages:</h6> -->
-                    <!-- <a class="collapse-item" href="404.html">404 Page</a> -->
-                    <!-- <a class="collapse-item active" href="blank.html">Blank Page</a> -->
-                <!-- </div> -->
-            <!-- </div> -->
-        <!-- </li> -->
+        <li class="nav-item <?php echo ($activePage === 'students') ? 'active' : '';?>">
+            <a class="nav-link" href="./students.php" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+                <i class="fas fa-fw fa-graduation-cap"></i>
+                <span>Student</span>
+            </a>
+            <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Humanities:</h6>
+                    <a class="collapse-item" href="./bca.php">BCA</a>
+                    <div class="collapse-divider"></div>
+                    <h6 class="collapse-header">Management:</h6>
+                    <a class="collapse-item" href="./bim.php">BIM</a>
+                    <div class="collapse-divider"></div>
+                    <h6 class="collapse-header">Science:</h6>
+                    <a class="collapse-item" href="./bsccsit.php">Bsc CSIT</a>
+                </div>
+            </div>
+        </li>
         
         <!-- Nav Item - Students -->
-        <li class="nav-item <?php echo ($activePage === 'students') ? 'active' : '';?>">
+        <!-- <li class="nav-item">
             <a class="nav-link" href="./students.php">
                 <i class="fas fa-fw fa-graduation-cap"></i>
                 <span>Students</span></a>
-        </li>
+        </li> -->
         
         <!-- Nav Item - Tables -->
         <!-- <li class="nav-item">
